651b5337f0e3aa4f9724d78fcaced103a2d4dcfe
Santhosh0077 <141298674+Santhosh0077@users.noreply.github.com>
2023-12-17 21:03:32 +0530

    Update calculate_gst.py

--- calculate_gst.py
+++ calculate_gst.py
@@ -1,6 +1,6 @@
 item=input("enter item name:")
 price=float(input("enter the price of the item:"))
-print('---------invoice---------')
+print('---------INVOICE---------')
 if price<10000:
     gst=5
 elif price>1000 and price<50000:
@@ -9,8 +9,8 @@ else :
     gst = 25
 gst_amount=(price * gst)/100
 net_amout=(gst_amount + price)
-print(f'item name={item}')
-print(f'price ={price}')
-print(f'gst is {gst}%')
-print(f'gst amount={gst_amount}')
-print(f'net_amount={net_amout}')
+print(f'ITEM NAME={item}')
+print(f'PRICE ={price}')
+print(f'GST is {gst}%')
+print(f'GST AMOUNT={gst_amount}')
+print(f'Net AMOUNT={net_amout}')
